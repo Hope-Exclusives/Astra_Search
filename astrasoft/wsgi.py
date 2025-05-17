992c48bd54310582673d521b07c982a49a29d274
hopedevelopers254 <design.hopedevelopers@gmail.com>
2025-05-17 13:56:14 +0300

    home page bugger menu added

--- astrasoft/wsgi.py
+++ astrasoft/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for astrasearch project.
+WSGI config for astrasoft project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'astrasearch.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'astrasoft.settings')
 
 application = get_wsgi_application()
